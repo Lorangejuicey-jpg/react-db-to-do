b961b63a9044effad8a9f39f499ff6136a7a8dc5
LorangeJuicey <60689696+LorangeJuicey@users.noreply.github.com>
2022-06-21 12:11:53 +0200

    laptop update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { db } from './firebase.js';
-import { collection , query, orderBy , onSnapshot, addDoc,serverTimestamp} from 'firebase/firestore';
-import './App.css';
-import Content from './content';
-import RouterPage from './router';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { db } from "./firebase.js";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import "./App.css";
+import Content from "./content";
+import RouterPage from "./router";
+import Login from "./components/login";
+import Register from "./components/Registreren";
+import Reset from "./components/Reset";
+import Dashboard from "./components/Reset";
 
 function App() {
-return (
-<div className="App">
-<Router>
-    <Routes>
-        <Route exact path="/" element={<Content />} />
-        <Route exact path="/router" element={<RouterPage />} />
-    </Routes>
-</Router>
-</div>
-);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/content" element={<Content />} />
+          <Route exact path="/router" element={<RouterPage />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+// import { getFirestore } from "firebase/firestore";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -8,10 +25,82 @@ const firebaseConfig = {
   projectId: "react-to-do-ca605",
   storageBucket: "react-to-do-ca605.appspot.com",
   messagingSenderId: "491263464077",
-  appId: "1:491263464077:web:509296f9132375da2fc8c4"
+  appId: "1:491263464077:web:509296f9132375da2fc8c4",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
-export { db }
+// export { db };
+
+// Google authentication
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
